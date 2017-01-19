0802f13177fcdc8970c00341d7e870bdcf66217b
davidkorff <davidkorff@gmail.com>
2017-01-19 11:20:41 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,17 +1,19 @@
 # Implement your procedural solution here!
 
 def largest_palindrome_product
-  array = []
+  highest = 0
 
   for x in (100..999) do
     for y in (x..999) do
       if is_palindrome?(x*y)
-        array << x*y
+        if highest<x*y
+          highest = x*y
+        end
       end
     end
   end
 
-  return array.max
+  return highest
 
 end
 
@@ -21,3 +23,5 @@ end
 def is_palindrome?(product)
   product.to_s == product.to_s.reverse
 end
+
+puts largest_palindrome_product
